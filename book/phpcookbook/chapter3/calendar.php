1720c5bb6cb98b416767f3c01bb47b300df50c7f
zhangbaoya <zhangbaoya@fangdd.com>
2016-08-05 17:18:45 +0800

    add calendar

---
+++ book/phpcookbook/chapter3/calendar.php
@@ -0,0 +1,115 @@
+<?php
+class Calendar {
+	protected $monthToUse;
+	protected $prepared = false;
+	protected $days = array ();
+	public function __construct($month, $year) {
+		$this->monthToUse = DateTime::createFromFormat ( 'Y-m|', sprintf ( "%04d-%02d", $year, $month ) );
+		// print_r($this->monthToUse);
+		$this->prepare ();
+		// print_r ( $this->days );
+	}
+	protected function prepare() {
+		foreach ( array (
+				'Su',
+				'Mo',
+				'Tu',
+				'We',
+				'Th',
+				'Fr',
+				'Sa' 
+		) as $dow ) {
+			$endOfRow = ($dow == 'Sa');
+			$this->days [] = array (
+					'type' => 'dow',
+					'label' => $dow,
+					'endOfRow' => $endOfRow 
+			);
+		}
+		// 星期中的第几天 0（表示星期天）到 6（表示星期六）
+		for($i = 0, $j = $this->monthToUse->format ( 'w' ); $i < $j; $i ++) {
+			$this->days [] = [ 
+					'type' => 'blank' 
+			];
+		}
+		
+		$today = date ( 'Y-m-d' );
+		// 给定月份所应有的天数 28 到 31
+		$days = new DatePeriod ( $this->monthToUse, new DateInterval ( 'P1D' ), $this->monthToUse->format ( 't' ) - 1 );
+		foreach ( $days as $day ) {
+			$isToday = ($day->format ( 'Y-m-d' ) == $today);
+			$endOfRow = ($day->format ( 'w' ) == 6);
+			$this->days [] = array (
+					'type' => 'day',
+					'label' => $day->format ( 'j' ),
+					'today' => $isToday,
+					'endOfRow' => $endOfRow 
+			);
+		}
+		if (! $endOfRow) {
+			for($i = 0, $j = 6 - $day->format ( 'w' ); $i < $j; $i ++) {
+				$this->days [] = array (
+						'type' => 'blank' 
+				);
+			}
+		}
+	}
+	// output html
+	public function html($opts = []) {
+		if (! isset ( $opts ['id'] )) {
+			$opts ['id'] = 'calendar';
+		}
+		if (! isset ( $opts ['month_link'] )) {
+			$opts ['month_link'] = '<a href="' . htmlentities ( $_SERVER ['PHP_SELF'] ) . '?' . 'month=%d&amp;year=%d">%s</a>';
+		}
+		$classes = [ ];
+		foreach ( array (
+				'prev',
+				'month',
+				'next',
+				'weekday',
+				'blank',
+				'day',
+				'today' 
+		) as $classe ) {
+			if (isset ( $opts ['class'] ) && isset ( $opts ['class'] [$classe] )) {
+				$classes [$classe] = $opts ['class'] [$classe];
+			} else {
+				$classes [$classe] = $classe;
+			}
+		}
+	}
+		// output cmd
+	function text() {
+		$lineLength = strlen ( 'Su Mo Tu We Th Fr Sa' );
+		$header = $this->monthToUse->format ( 'F Y' );
+		$headerSpacing = floor ( ($lineLength - strlen ( $header )) / 2 );
+		
+		$text = str_repeat ( ' ', $headerSpacing ) . $header . PHP_EOL;
+		foreach ( $this->days as $i => $day ) {
+			switch ($day ['type']) {
+				case 'dow' :
+					$text .= sprintf ( '% 2s', $day ['label'] );
+					break;
+				case 'blank' :
+					$text .= ' ';
+					break;
+				case 'day' :
+					$text .= sprintf ( '% 2d', $day ['label'] );
+					break;
+				default :
+					;
+					break;
+			}
+			$text.=(isset($day['endOfRow'])&&$day['endOfRow'])?PHP_EOL:' ';
+		}
+		if($text[strlen($text)-1]!=PHP_EOL){
+			$text.=PHP_EOL;
+		}
+		return $text;
+	}
+	
+}
+
+$ca = new Calendar ( 8, 2016 );
+echo $ca->text();
--- book/phpcookbook/chapter3/date.php
+++ book/phpcookbook/chapter3/date.php
@@ -26,3 +26,76 @@ $datetime2 = new DateTime('2009-10-13');
 $interval = $datetime1->diff($datetime2);
 echo $interval->format('%R%a days');
 echo PHP_EOL;
+
+
+$birthday = new DateTime('March 10, 1975');
+$human_gestation =new DateInterval('P40W');
+$birthday->sub($human_gestation);
+print $birthday->format(DateTime::ISO8601);
+echo PHP_EOL;
+
+$elephant_gestation = new DateInterval('P365D');
+$birthday->add($elephant_gestation);
+print $birthday->format(DateTime::ISO8601);
+echo PHP_EOL;
+
+$year = 2016;
+$when = new DateTime("November 1,$year");
+print_r($when);
+echo PHP_EOL;
+if($when->format('D')!='Mon'){
+	$when->modify("next Monday");
+}
+print_r($when);
+echo PHP_EOL;
+$when->modify("next Tuesday");
+print_r($when);
+echo PHP_EOL;
+
+$nowInNewYork = new DateTime('now',new DateTimeZone('America/New_York'));
+$nowInCalifornia = new DateTime('now',new DateTimeZone('America/Los_Angeles'));
+printf("New York:%s  \nCalifornia:%s",$nowInNewYork->format(DateTime::RFC850),$nowInCalifornia->format(DateTime::RFC850));
+echo PHP_EOL;
+
+function microtime_float()
+{
+	list($usec, $sec) = explode(" ", microtime());
+	return ((float)$usec + (float)$sec);
+}
+echo microtime();
+echo PHP_EOL;
+echo microtime(true);
+echo PHP_EOL;
+echo microtime_float();
+echo PHP_EOL;
+print_r(gettimeofday());
+echo PHP_EOL;
+$datetime=new DateTime();
+print_r($datetime);
+echo PHP_EOL;
+echo $datetime->format('u');
+echo PHP_EOL;
+
+//DatePeriod
+$start =new DateTime('August 1, 2014');
+$end = new DateTime('September 1, 2014');
+$interval =new DateInterval('P1D');
+$range1 =new DatePeriod($start, $interval, $end);
+foreach ($range1 as $d) {
+	//print "day:".$d->format('d');
+	//echo PHP_EOL;
+}
+//过去六个月
+$end =new DateTime();
+$start = new DateTime('-6 months ');
+echo $start->format('m');
+echo PHP_EOL;
+$interval =new DateInterval('P1M');
+$range3 =new DatePeriod($start, $interval, $end);
+//print_r($range3);
+foreach ($range3 as $m) {
+	print "day:".$m->format('m');
+	echo PHP_EOL;
+}
+echo PHP_EOL;
+print_r(cal_info());
