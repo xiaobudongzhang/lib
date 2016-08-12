9e65a5e4788ef1acacbe87351d069de387368139
zhangbaoya <zhangbaoya@fangdd.com>
2016-08-12 14:44:40 +0800

    timeout test

--- book/phpcookbook/rest/timeout.php
+++ book/phpcookbook/rest/timeout.php
@@ -1,22 +1,27 @@
 <?php
-$url='https://getcomposer.org/download/1.2.0/composer.phar';
+//phpinfo();
+$url='http://m.fangdd.net/test/timeout/set?time=5';
+ini_set('default_socket_timeout', 1);
+//file_get_contents($url);
+//die;
 //ini
 /* ini_set('default_socket_timeout', 12);
 file_get_contents($url); */
 
-//curl
-/* $c=curl_init($url);
+/* //curl
+$c=curl_init($url);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, false);
-curl_setopt($c, CURLOPT_TIMEOUT, 3);
+//curl_setopt($c, CURLOPT_TIMEOUT, 1);
 $page=curl_exec($c);
 $error=curl_errno($c);
 curl_close($c);
-print_r($error); */
+//print_r($page);
+die; */
 
 //stream
  $stream=fopen($url, 'rb');
 stream_set_blocking($stream, TRUE);
-$set=stream_set_timeout($stream,1);
+//$set=stream_set_timeout($stream,1);
 var_dump($set);
 $response=stream_get_contents($stream);
 $info = stream_get_meta_data($stream);
