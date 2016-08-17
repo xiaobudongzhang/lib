81a517dae3f52f1c60869b9a47d0c2c91820b2b0
xiaobudongzhang <xiaobudongzhang@gmail.com>
2016-08-17 23:11:53 +0800

    add stream_select

--- book/phpcookbook/service/stream.php
+++ book/phpcookbook/service/stream.php
@@ -1,28 +1,51 @@
 <?php
-/* $read = fsockopen('127.0.0.1',
-		8009,
-		$errno,
-		$errstr
-		);
-$read2 = fsockopen('127.0.0.1',
-		8010,
-		$errno,
-		$errstr
-		); */
-$read=stream_socket_client("127.0.0.1:8009", $errno, $errstr, 1000,
-		STREAM_CLIENT_ASYNC_CONNECT|STREAM_CLIENT_CONNECT);
-$read2=stream_socket_client("127.0.0.1:8010", $errno, $errstr, 1000,
-		STREAM_CLIENT_ASYNC_CONNECT|STREAM_CLIENT_CONNECT);
-/* $tv_sec=0;
-$tv_usec=1000;
-$out = "GET / HTTP/1.1\r\n";
-//$out .= "Host: www.example.com\r\n";
-$out .= "Connection: Close\r\n\r\n";
-fwrite($read, $out);
-while (!feof($read)) {
-	echo (fgets($read,1024));
+$timeout=0;
+$mitime=500000;
+$result=[];
+$sockets=[];
+$convenient_read_block=8192;
+
+$delay=15;
+$id=0;
+while($delay>0){
+	$s=stream_socket_client(
+	"127.0.0.1:8009",
+	$errorno,
+	$errstr,
+	STREAM_CLIENT_ASYNC_CONNECT|STREAM_CLIENT_CONNECT
+	);
+	if($s){
+	$sockets[$id++]=$s;
+	$http_message="GET /demonstration/delay?delay=" .$delay . " HTTP/1.0\r\nHost: phaseit.net\r\n\r\n"; 
+	fwrite($s,$http_message);
+	}else{
+	
+	}
+$delay-=3;
 }
-fclose($read); */
-//var_dump(file_get_contents($read));
-$r=[$read,$read2];
-$num=stream_select($r, null, null, $tv_sec, $tv_usec );
+
+//var_dump($sockets);
+
+while(count($sockets)){
+	$read=$sockets;
+	 stream_select($read, $w=null, $e=null, $timeout,$mitime);
+	 if(count($read)){
+	 
+		foreach($read as $r){
+			      $id=array_search($r, $sockets); 
+			      $data=fread($r,$convenient_read_block);
+			      if(strlen($data)==0){
+			       echo "Stream " . $id . " closes at " . date('h:i:s') . ".\n";
+                        fclose($r); 
+                        unset($sockets[$id]); 
+			      }else{
+			       $result[$id] = $data; 
+			      }
+		}
+	 } else{
+	 
+	 }
+}
+
+
+print_r($result);
